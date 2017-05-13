590247112c3ca733eca52d5750c1f7b1244c6125
tactreconx <league@WTS-MM-11.local>
2017-05-13 11:27:33 -0700

    Everybody is amazing

---
+++ src/level0vapsi/Morningzombie.java
@@ -0,0 +1,19 @@
+package level0vapsi;
+
+import javax.swing.JOptionPane;
+
+public class Morningzombie {
+public static void main(String[] args) {
+	System.out.println("starbuks is the bomb!");
+JOptionPane.showMessageDialog(null, "brush your teeth");
+JOptionPane.showMessageDialog(null, "change clothes");
+JOptionPane.showMessageDialog(null, "eat breakfast");
+JOptionPane.showMessageDialog(null, "brush teeth again");
+JOptionPane.showMessageDialog(null, "go to school");
+JOptionPane.showMessageDialog(null, "give an apple to teacher");
+JOptionPane.showMessageDialog(null, "don't eat friends brains");
+
+
+
+}
+}
---
+++ src/level0vapsi/knockknock.java
@@ -0,0 +1,21 @@
+package level0vapsi;
+
+import javax.swing.JOptionPane;
+
+public class knockknock {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("knock knock");
+JOptionPane.showInputDialog("Leaf!");
+JOptionPane .showMessageDialog(null, "Leaf the house, you’re not the owner anymore!");
+
+
+
+
+
+
+
+
+
+
+}
+}
