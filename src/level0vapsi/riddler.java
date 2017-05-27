9b7776987db32dc758534cc4b5b6e292e94dabbb
tactreconx <league@WTS-MM-11.local>
2017-05-27 11:28:01 -0700

    We did riddles

---
+++ src/level0vapsi/riddler.java
@@ -0,0 +1,24 @@
+package level0vapsi;
+
+import javax.swing.JOptionPane;
+
+public class riddler {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("You can see me in water, but I never get wet. What am I?");
+JOptionPane.showMessageDialog(null, "a reflection");
+		JOptionPane.showInputDialog("A man is trapped in a room. The room has only two possible exits: two doors. Through the first door there is a room constructed from magnifying glass. The blazing hot sun instantly fries anything or anyone that enters. Through the second door there is a fire-breathing dragon. How does the man escape?");
+	JOptionPane.showMessageDialog(null, "He waits until night time and then goes through the first door.");
+	JOptionPane.showInputDialog("A king has no sons, no daughters, and no queen. For this reason he must decide who will take the throne after he dies. To do this he decides that he will give all of the children of the kingdom a single seed. Whichever child has the largest, most beautiful plant will earn the throne; this being a metaphor for the kingdom.At the end of the contest all of the children came to the palace with their enormous and beautiful plants in hand. After he looks at all of the children's pots, he finally decides that the little girl with an empty pot will be the next Queen. Why did he choose this little girl over all of the other children with their beautiful plants?");
+	JOptionPane.showMessageDialog(null, "The king gave them all fake seeds and the little girl was the only honest child who didn't switch seeds");
+	JOptionPane.showInputDialog("An old man wanted to leave all of his money to one of his three sons, but he didn't know which one he should give it to. He gave each of them a few coins and told them to buy something that would be able to fill their living room. The first man bought straw, but there was not enough to fill the room. The second bought some sticks, but they still did not fill the room. The third man bought two things that filled the room, so he obtained his father's fortune. What were the two things that the man bought?");
+	JOptionPane.showMessageDialog(null, "The wise son bought a candle and a box of matches. After lighting the candle, the light filled the entire room.");
+	JOptionPane.showInputDialog("A thief enters a shop and threatens the clerk, forcing him to open the safe. The clerk says, "The code for the safe is different every day, and if you hurt me you'll never get the code". But the thief manages to guess the code on his own How did he do it?");
+	JOptionPane.showMessageDialog(null, "The code is "different." The clerk told him this.");
+	
+	
+	
+	
+	
+	}
+
+}
---
+++ src/level0vapsi/warmup.java
@@ -0,0 +1,20 @@
+package level0vapsi;
+
+import javax.swing.JOptionPane;
+
+public class warmup {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "hello");
+JOptionPane.showMessageDialog(null, "my favorite color is red");
+JOptionPane.showMessageDialog(null, "you are amazing");
+JOptionPane.showMessageDialog(null, "my favorite animal is a wolf");
+JOptionPane.showMessageDialog(null, "i like food");
+JOptionPane.showMessageDialog(null, "i like cats and dogs");
+JOptionPane.showMessageDialog(null, "i like knots berry farm");
+
+
+
+
+
+}
+}
